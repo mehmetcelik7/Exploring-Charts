785aae9f62b1d678eddfb88e0dc93679379cbbbc
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-04 18:46:14 +0300

    Initial Commit

---
+++ Exploring Charts/Exploring_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exploring_ChartsApp.swift
+//  Exploring Charts
+//
+//  Created by mehmet Çelik on 4.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Exploring_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
